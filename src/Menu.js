26203380db7466e2bfc9bd2420291153fd319104
Eduardo Felipe <erfelipe@yahoo.com.br>
2022-11-30 18:51:09 -0300

    inicio do projeto

---
+++ src/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Menu.css"
+
+export default function Menu() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><Link className='itemMenu' to="/">Pag 1 - home</Link> </li>
+                <li><Link className='itemMenu' to="/usuarios">Clientes</Link></li>
+                <li><Link className='itemMenu' to="/sobre"> Pag 3 - sobre </Link></li>
+                <li><Link className='itemMenu' to="/pessoas"> Pessoas </Link></li>
+            </ul> 
+        </nav>
+    </header>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Component1 from './Component1';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from './Menu';
+import Sobre from './Sobre';
+import Usuarios from './Usuarios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Menu />
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="usuarios" element={<Component1 />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="pessoas" element={<Usuarios/> } /> 
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
